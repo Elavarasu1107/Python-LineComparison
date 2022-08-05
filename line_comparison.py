b23d5ed858c040b6ce099f7ef0be030d2d42a54c
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-08-05 17:09:18 +0530

    [Elavarasu] UC3-Line Comparison

--- line_comparison.py
+++ line_comparison.py
@@ -1,33 +1,74 @@
 import math
+import logging
+
+logging.basicConfig(filename='line_log.log', encoding='utf-8', level=logging.DEBUG,
+                    format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logger = logging.getLogger()
 
 
-def line_comparison(x1, y1, x2, y2):
-    """
-    This function calculates length of a line
-    :param x1: Coordinate x1 of a line
-    :param y1: Coordinate y1 of a line
-    :param x2: Coordinate x2 of a line
-    :param y2: Coordinate y2 of a line
-    :return: Length of a line
-    """
-    line_length = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
-    print("Line length: ", line_length)
-    return line_length
+class Line:
+
+    def __init__(self, details_dict):
+        self.x1 = details_dict.get("x1")
+        self.y1 = details_dict.get("y1")
+        self.x2 = details_dict.get("x2")
+        self.y2 = details_dict.get("y2")
+
+    def length_finder(self):
+        """
+        This function calculates length of a line
+        :return: Number
+        """
+        try:
+            line_length = math.sqrt(math.pow(self.x2 - self.x1, 2) + math.pow(self.y2 - self.y1, 2))
+            return line_length
+        except Exception as ex:
+            logger.exception(ex)
+
+    def __eq__(self, line_obj):
+        try:
+            return self.length_finder() == line_obj.length_finder()
+        except Exception as ex:
+            logger.exception(ex)
+
+    def __gt__(self, line_obj):
+        try:
+            return self.length_finder() > line_obj.length_finder()
+        except Exception as ex:
+            logger.exception(ex)
+
+    def __lt__(self, line_obj):
+        try:
+            return self.length_finder() < line_obj.length_finder()
+        except Exception as ex:
+            logger.exception(ex)
 
 
-if __name__ == '__main__':
-    length = []
-    for i in range(0, 2):
+def add_line():
+    try:
         x1 = int(input("Enter value of X1: "))
         y1 = int(input("Enter value of Y1: "))
         x2 = int(input("Enter value of X2: "))
         y2 = int(input("Enter value of Y2: "))
-        length.append(line_comparison(x1, y1, x2, y2))
-    if length[0] > length[1]:
-        print("Line 1 is larger than Line 2")
-    elif length[1] > length[0]:
-        print("Line 2 is larger than Line 1")
-    else:
-        print("Both lines are Equal")
+        data_dict = {"x1": x1, "y1": y1, "x2": x2, "y2": y2}
+        line = Line(data_dict)
+        return line
+    except Exception as ex:
+        logger.exception(ex)
 
 
+if __name__ == '__main__':
+    try:
+        print("Enter data for line One: ")
+        line_one = add_line()
+        print("Enter data for line Two: ")
+        line_two = add_line()
+
+        if line_one > line_two:
+            print("Line 1 is larger than Line 2 ")
+        elif line_one < line_two:
+            print("Line 2 is larger than Line 1 ")
+        elif line_two == line_two:
+            print("Both lines are equal")
+    except Exception as e:
+        logger.exception(e)
